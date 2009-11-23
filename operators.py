76dae01af89bdc8a8dc7b24f0f0a7c731eaa44b4
PreludeAndFugue <gdrummondk@gmail.com>
2009-11-23 16:34:54 +0000

    Started implementation of derivative operators.

--- operators.py
+++ operators.py
@@ -1,21 +1,81 @@
-from sympy import Function, Basic
+from sympy import Symbol, Function, conjugate, S
 
-D = Function('D')
-Delta = Function ('Delta')
-delta = Function('delta')
-deltab = Function('deltab')
+#D = Function('D')
+#Delta = Function ('Delta')
+#delta = Function('delta')
+#deltab = Function('deltab')    
 
 class DerivativeOperator(Function):
     
     nargs = 1
-    
+
     @classmethod
     def eval(cls, arg):
-        # if arg is a constant return 0
-        # if arg is ...
-        pass
-        
-    def _eval_conjugate(self):
-        pass
-        
+        if arg.is_Number:
+            return S.Zero
+        if arg.is_negative:
+            return -cls(-arg)
+        if arg.could_extract_minus_sign():
+            return -cls(-arg)
+        if arg.is_Add:
+            result = 0
+            for val in arg.args:
+                result += cls(val)
+            return result
+        if arg.is_Pow:
+            x, n = arg.as_base_exp()
+            return n*cls(x**(n - 1))
+        if arg.is_Mul:
+            # TODO: this has to be generalised to arbitrary number of args
+            x, y = arg.args
+            return x*cls(y) + y*cls(x)
+            
+            
+class D(DerivativeOperator):
     
+    def _eval_conjugate(self):
+        # for real derivative operator
+        return self.func(conjugate(self.args[0]))
+        
+        
+class Delta(DerivativeOperator):
+
+    def _eval_conjugate(self):
+        # for real derivative operator
+        return self.func(conjugate(self.args[0]))
+
+        
+class delta(DerivativeOperator):
+    
+    def _eval_conjugate(self):
+        return deltab(conjugate(self.args[0]))
+
+        
+class deltab(DerivativeOperator):
+
+    def _eval_conjugate(self):
+        return delta(conjugate(self.args[0]))
+
+        
+def main(): 
+    x = Symbol('x', real = True)
+    y = Symbol('y')
+    z = Symbol('z')
+    
+    print conjugate(D(x**2))
+    print 'D(5) =', D(5)
+    print 'D(-x) = ', D(-x)
+    print 'D(x + y) = ', D(x + y)
+    print D(x - y)
+    print D(x**2)
+    print D(x*y)
+    #print D(x*y*z)
+    print
+    print conjugate(delta(x))
+    print D(Delta(x**2))
+    print
+    print
+    print Delta.nargs
+
+if __name__ == '__main__':
+    main()
